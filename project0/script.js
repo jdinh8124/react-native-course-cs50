4cc34aa29d90820d58318118b76e81b90ad05d36
James <mrjames8124@gmail.com>
2020-03-23 15:29:46 -0700

    creating input dynamically

--- project0/script.js
+++ project0/script.js
@@ -11,6 +11,13 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 classNames.TODO_TEXT = document.getElementById
 
+let input = document.createElement("input");
+input.type = "text";
+// input.className = "container"; // set the CSS class
+itemCountSpan.appendChild(input);
+
 function newTodo() {
-  alert('New TODO button clicked!')
+  let num =  parseInt(itemCountSpan.innerText);
+  num++;
+  itemCountSpan.innerText = num;
 }
