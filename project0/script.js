e9e0fdca5b4b1d5f11e91b33602f94425fb44769
James <mrjames8124@gmail.com>
2020-03-24 11:50:52 -0700

    completed project 0

--- project0/script.js
+++ project0/script.js
@@ -1,22 +1,9 @@
-const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
-}
-
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 classNames.TODO_TEXT = document.getElementById
 
-// let input = document.createElement("input");
-// input.type = "text";
-// // input.className = "css-class-name"; // set the CSS class
-// let buttonOne = document.getElementsByClassName("container center")
-// console.log(buttonOne)
-// buttonOne.appendChild(input);
 
 function newTodo() {
   let num =  parseInt(itemCountSpan.innerText);
